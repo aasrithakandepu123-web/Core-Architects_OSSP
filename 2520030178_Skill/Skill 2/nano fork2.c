545c16ba24d1f52df1ae0def1ba958698d0d323b
aasritha kandepu <aasrithakandepu123@gmail.com>
2026-08-24 11:24:32 +0530

    Create nano fork2.c

---
+++ 2520030178_Skill/Skill 2/nano fork2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char command[100];
+
+    while(1)
+    {
+        printf("Enter a Command\n");
+        fgets(command, sizeof(command), stdin);
+
+        command[strcspn(command, "\n")] = '\0';
+
+        if(strcmp(command, "exit") == 0)
+        {
+            printf("Exiting shell...\n");
+            break;
+        }
+
+        printf("    Command received\n");
+        printf("%s\n", command);
+    }
+
+    return 0;
+}
