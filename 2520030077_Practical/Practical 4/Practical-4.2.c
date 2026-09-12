2d7d85e339e5d8323b9ce492e457b825da7c63fb
bindhu-java <pennetibindhumadhavi@gmail.com>
2026-09-12 19:35:18 +0530

    Add process creation example using fork
    Implement basic process creation using fork in C.

---
+++ 2520030077_Practical/Practical 4/Practical-4.2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+{
+int pid;
+pid=fork();
+if(pid==0)
+{
+printf("Child process %d\n",getpid());
+exit(0);
+}
+else
+{
+printf("Parent process %d\n",getpid());
+//wait(NULL);
+printf("Parent process completed");
+}
+}
+
